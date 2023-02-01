ea20a67e344c07dee3e65d337a94695a2ec2bf28
Irshad Nazree <69236341+irshadnazree@users.noreply.github.com>
2023-02-01 23:31:33 +0800

    V1.03 - edit payment actvity + layout

--- app/src/main/java/ftmk/bitp3453/OrderApp/PaymentActivity.java
+++ app/src/main/java/ftmk/bitp3453/OrderApp/PaymentActivity.java
@@ -3,6 +3,7 @@ package ftmk.bitp3453.OrderApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class PaymentActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class PaymentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        Toast.makeText(PaymentActivity.this,"See you again!", Toast.LENGTH_SHORT).show();
     }
 }
